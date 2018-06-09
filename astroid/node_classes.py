62127d8c70ee8990fc0e837ed731c4dca44dbca8
Nick Drozd <nicholasdrozd@gmail.com>
2018-06-09 10:10:20 -0500

    name nodes extra

--- astroid/node_classes.py
+++ astroid/node_classes.py
@@ -1276,9 +1276,6 @@ class Name(mixins.NoChildrenMixin, LookupMixIn, NodeNG):
     def _get_name_nodes(self):
         yield self
 
-        for child_node in self.get_children():
-            yield from child_node._get_name_nodes()
-
 
 class Arguments(mixins.AssignTypeMixin, NodeNG):
     """Class representing an :class:`ast.arguments` node.
